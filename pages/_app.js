eefd5af5bccae98c4415d936ca84669fd4c31aa2
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-09 20:54:21 -0600

    .
    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import 'bootswatch/dist/cosmo/bootstrap.min.css'
-
-//import 'animate.css/animate.min.css'
+import 'animate.css/animate.min.css'
 import '../global.css'
 
 export default function MyApp({ Component, pageProps }) {
